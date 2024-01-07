32b81978053a11897314cd64a7fa3a4594d731cb
Sid bhai <145905667+gssbotwa@users.noreply.github.com>
2024-01-07 10:53:40 +0530

    update api.js

--- api.js
+++ api.js
@@ -1,51 +1,175 @@
 const express = require('express');
-const ytSearch = require('yt-search');
+const { Boom } = require('@hapi/boom');
+const NodeCache = require('node-cache');
+const fs = require('fs');
+const Pino = require('pino');
+const axios = require('axios');
+const baileys = require('@whiskeysockets/baileys');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000; // You can change the port as needed
 
-app.get('/api', async (req, res) => {
+app.use(express.json());
+
+const { useMultiFileAuthState, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore, PHONENUMBER_MCC, delay } = baileys;
+
+const styledText = (text, fgColorCode, bgColorCode, isBold) => {
+  const attributes = [fgColorCode, bgColorCode, isBold];
+  const attributeString = attributes.join(';');
+  return `\x1b[${attributeString}m${text}\x1b[0m`;
+};
+
+const getInput = (text = 'Enter Your Phone number: ') => {
+  let input = prompt(styledText(text, 32, 40, 1));
+  let res = input ? input.replace(/[^0-9]/g, '') : '';
+  if (res && !isNaN(res) && res.length > 7) {
+    return res;
+  } else {
+    console.log(styledText('YOU ENYERED AN INVALID PHONE NUMBER ', 31, 40, 1));
+    process.exit(0);
+  }
+};
+
+const remove = async (dir) => {
   try {
-    const searchQuery = req.query.search;
+    if (fs.existsSync(dir)) {
+      await fs.rmdirSync(dir, { recursive: true });
+    }
+  } catch {}
+};
 
-    if (!searchQuery && searchQuery !== '') {
-      res.json({ error: 'Please provide a valid search query.' });
-      return;
+var phoneNumber = getInput();
+
+let dirName = `sessions/${phoneNumber}'s_info`;
+
+const store = makeInMemoryStore({ logger: Pino({ level: 'silent' }).child({ level: 'silent' }) });
+
+async function start() {
+  process.on('unhandledRejection', (err) => console.error(err));
+
+  const { state, saveCreds } = await useMultiFileAuthState(`./${dirName}`);
+  const msgRetryCounterCache = new NodeCache();
+
+  const gss = baileys.default({
+    logger: Pino({ level: 'silent' }).child({ level: 'silent' }),
+    printQRInTerminal: false,
+    auth: {
+      creds: state.creds,
+      keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: 'silent' }).child({ level: 'silent' })),
+    },
+    browser: ['Chrome (Linux)', '', ''],
+    markOnlineOnConnect: false,
+    generateHighQualityLinkPreview: true,
+    getMessage: async (key) => {
+      let jid = jidNormalizedUser(key.remoteJid);
+      let msg = await store.loadMessage(jid, key.id);
+      return msg?.message || '';
+    },
+    msgRetryCounterCache,
+    defaultQueryTimeoutMs: undefined,
+  });
+
+  store.bind(gss.ev);
+
+  if (!gss.authState.creds.registered) {
+    setTimeout(async () => {
+      let code = await gss.requestPairingCode(phoneNumber);
+      code = code?.match(/.{1,4}/g)?.join('-') || code;
+      console.log(styledText(`\n\nYour Pairing Code:`, 37, 33, 1) + '\t' + styledText(code, 31, 46, 1) + '\n');
+      console.log();
+    }, 3000);
+  }
+
+  gss.ev.on('connection.update', async (update) => {
+    const { lastDisconnect, connection, qr } = update;
+    if (connection) {
+      //console.info(`Connection Status : ${connection}`)
     }
 
-    const searchResults = await performSearch(searchQuery);
-    res.json(searchResults);
+    if (connection === 'close') {
+      let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+      if (reason === DisconnectReason.badSession) {
+        console.log(`Bad Session File, Please Delete Session and Scan Again`);
+        process.exit(0);
+      } else if (reason === DisconnectReason.connectionClosed) {
+        console.log('Connection closed, reconnecting....');
+        await start();
+      } else if (reason === DisconnectReason.connectionLost) {
+        console.log('Connection Lost from Server, Please run again!');
+        process.exit(1);
+      } else if (reason === DisconnectReason.connectionReplaced) {
+        console.log('Connection Replaced, Another New Session Opened, Please Close Current Session First');
+        process.exit(1);
+      } else if (reason === DisconnectReason.loggedOut) {
+        console.log(`Device Logged Out, Please Scan Again And Run.`);
+        process.exit(1);
+      } else if (reason === DisconnectReason.restartRequired) {
+        await start();
+      } else if (reason === DisconnectReason.timedOut) {
+        console.log('Connection TimedOut, Reconnecting...');
+        await start();
+      } else if (reason === DisconnectReason.multideviceMismatch) {
+        console.log('Multi device mismatch, please scan again');
+        process.exit(0);
+      } else {
+        console.log(reason);
+        process.exit(0);
+      }
+    }
+
+    if (connection === 'open') {
+      console.log('Connected');
+      console.log(styledText('DEVICE LOGGED IN 100% ', 31, 40, 1));
+      let user = gss.user.id;
+
+      await delay(3000);
+      let unique = await fs.readFileSync(__dirname + '/' + dirName + '/creds.json');
+      let Scan_ID = Buffer.from(unique).toString('base64');
+
+      console.log(`
+  ====================  SESSION ID  ===========================                   
+  SESSION-ID ==> ${Scan_ID}\n\n`);
+      console.log(styledText(`Don't provide your SESSION_ID to anyone otherwise that user can access your account.
+Thanks.`, 32, 40, 1), '\n-------------------  SESSION CLOSED   -----------------------');
+
+      let MESSAGES = `╔════◇
+  ║『 *THANKS FOR CHOOSING GSS_BOTWA* 』
+  ║ _The Upper text is your session id._
+  ╚════════════════════════╝
+  
+╭──────────────────────⦿   
+│       *⎙ S E S S I O N . O S 乂*
+╰┬─────────────────────⦿
+┌┤
+│♤ *Vɪsɪᴛ:* vibranium.clienturl.net
+│♤ *Yᴛᴜʙᴇ:* _youtube.com/infinite9452_
+│
+╰─────────────────────⦿
+  `;
+      await gss.sendMessage(user, { text: Scan_ID });
+      await gss.sendMessage(user, { text: MESSAGES });
+      try {
+        remove(dirName);
+      } catch {}
+      process.exit(1);
+    }
+  });
+
+  gss.ev.on('creds.update', saveCreds);
+}
+
+app.get('/login/:phoneNumber', async (req, res) => {
+  const { phoneNumber } = req.params;
+
+  try {
+    // Start the login process
+    await start();
+    res.status(200).send('Login process started. Check the console for details.');
   } catch (error) {
-    console.error(error);
-    res.json({ error: 'Error processing request.' });
+    res.status(500).send(`Error: ${error.message}`);
   }
 });
 
-async function performSearch(searchQuery) {
-  try {
-    if (!searchQuery || searchQuery.trim() === '') {
-      throw new Error('Invalid search query.');
-    }
-
-    const { videos } = await ytSearch(searchQuery);
-
-    // Extracting video information from the search results
-    const formattedResults = videos.slice(0, 10).map((video, i) => {
-      return {
-        title: video.title,
-        url: video.url,
-        uploadDate: video.ago, // Adding upload date
-        views: video.views, // Adding views count
-        duration: video.timestamp, // Adding video duration
-      };
-    });
-
-    return { type: 'search', data: formattedResults };
-  } catch (error) {
-    throw new Error('Error performing YouTube search.');
-  }
-}
-
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
